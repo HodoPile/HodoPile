9a9a9570dee6c02a81eefd625803a428bfa5c6d0
antoni <antoni.dev0@gmail.com>
2022-12-19 09:57:23 -0800

    add auth0 client to SPA, add login/logout/getProfile functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const UNSPLASH_ACCESS_KEY = "Vd8pXJDerwdrThr-HAsU9U8LHjAuWlFzi782_HYjlqU"
+const UNSPLASH_ACCESS_KEY = "SECRET"
 const UNSPLASH_BASE_URL = "https://api.unsplash.com/"
 const DELAY_INPUT = 1500
 
@@ -52,7 +52,8 @@ const renderResults = ( list ) => {
 const createCardElement = ( destinationObj ) => {
     const {
         urls: { small }, 
-        alt_description,description, 
+        alt_description,
+        description, 
         user: { first_name, last_name },
         tags,
         id
@@ -95,7 +96,71 @@ const getUnsplashImgURL = ( query ) => {
 
 inputTextField.addEventListener( "input", debounce( handleInputChange, DELAY_INPUT ))
 
+/* AUTHENTICATION */
+
+const runAuth = async () => {
+    const result = await fetch("http://localhost:3000/auth_config")
+    const auth_configs = await result.json()
+    const config = {
+            ...auth_configs,
+            authorizationParams: { redirect_uri: window.location.origin }
+        }
+    auth0.createAuth0Client( config )
+        .then( async ( auth0Client ) => {
+        const loginButton = document.getElementById("login");
+        const logoutButton = document.getElementById("logout");
+      
+        loginButton.addEventListener("click", (e) => {
+          e.preventDefault();
+          auth0Client.loginWithRedirect();
+        });
+      
+        if (location.search.includes("state=") && 
+            (location.search.includes("code=") || 
+            location.search.includes("error="))
+        ) {
+          await auth0Client.handleRedirectCallback();
+          window.history.replaceState({}, document.title, "/");
+        }
+        
+        logoutButton.addEventListener("click", (e) => {
+          e.preventDefault();
+          auth0Client.logout();
+        });
+      
+        const isAuthenticated = await auth0Client.isAuthenticated();
+        const userProfile = await auth0Client.getUser();
+        /*  
+            email: "hodo@user.com"
+            email_verified: false
+            name: "hodo@user.com"
+            nickname: "hodo"
+            picture: "https://s.gravatar.com/avatar/15e3d87fc8e38a8e03de36456d6a6547?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fho.png"
+            sub: "auth0|639ff283d8dbda8325b0e654"
+            updated_at: "2022-12-19T15:10:58.763Z"
+        */
+        const profileElement = document.getElementById("profile");
+      
+        if (isAuthenticated) {
+          profileElement.style.display = "block";
+          profileElement.innerHTML = `
+                  <p>${userProfile.name}</p>
+                  <img src="${userProfile.picture}" />
+                `;
+        } else {
+          profileElement.style.display = "none";
+        }
+        })
+        .catch( ( err ) => console.log( err ))
+}
+
+const getFavs = async () => {
+    const result = await fetch("http://localhost:3000/user")
+    const user = await result.json()
+}
+
 window.onload = ( e ) => {
     getUnsplashImgURL('africa')
+    runAuth()
+    getFavs()
 };
-  
